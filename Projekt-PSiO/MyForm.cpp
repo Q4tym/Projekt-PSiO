542e900ec774d08d1493b983335f6212aa375f87
Q4tym <hbergolec@gmail.com>
2025-04-05 16:38:13 +0200

    Kolejne zmiany tym razem w MyForm.cpp

--- Projekt-PSiO/MyForm.cpp
+++ Projekt-PSiO/MyForm.cpp
@@ -1,13 +1,13 @@
 #include "MyForm.h"
-using namespace System;
-using namespace System::Windows::Forms;
+//using namespace System;
+//using namespace System::Windows::Forms;
 
-[STAThread]
-void Main(array<String^>^ args)
+[System::STAThread]
+void Main(array<System::String^>^ args)
 
 {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
+	System::Windows::Forms::Application::EnableVisualStyles();
+	System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
 	ProjektPSiO::MyForm form;
-	Application::Run(% form);
+	System::Windows::Forms::Application::Run(% form);
 }
