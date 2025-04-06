77c523f6ca7cc7bf18e08ea78b16bd46c15d0767
Kamil <bijak.kamil@wp.pl>
2025-04-06 18:00:35 +0200

    testy

--- Projekt-PSiO/Klasy.h
+++ Projekt-PSiO/Klasy.h
@@ -23,15 +23,74 @@ public:
 class Nadawca : public Osoba,  public Adres
 {
 public:
-	Nadawca(std::string imie_N, std::string nazwisko_N std::string mail_N, std::string ulica_N, std::string miasto_N, std::string wojewodztwo_N, int kod_pocztowy_N, std::string kraj_N) 
+	virtual void dane() = 0;
+	/*
+	Nadawca(std::string imie_N, std::string nazwisko_N, std::string mail_N, std::string ulica_N, std::string miasto_N, std::string wojewodztwo_N, int kod_pocztowy_N, std::string kraj_N)
 	{
-		imie_N(imie), nazwisko_N(nazwisko), adres_mailowy_M(mail), ulica_N(ulica), miasto_N(miasto), wojewodztwo_N(wojewodztwo), kod_pocztowy_N(kod_pocztowy), kraj_N(kraj);
+		imie(imie_N); // imie_N = imie; imie(imie_N); imie(std::string imie_N)
+		nazwisko(nazwisko_N);
+		adres_mailowy(mail_N);
+		ulica(ulica_N), 
+		miasto(miasto_N), 
+		wojewodztwo(wojewodztwo_N), 
+		kod_pocztowy(kod_pocztowy_N), 
+		kraj(kraj_N);
+		
 	}
-	int numer_nadawczy;
+	*/
 };
 class Odbiorca : public Osoba, public Adres
 {
 public:
-	Odbiorca(std::string imie, std::string nazwisko, std::string mail, std::string ulica, std::string miasto, std::string wojewodztwo, int kod_pocztowy, std::string kraj) {}
-	int numer_nadawczy;
+	virtual void dane() = 0;
+	/*
+	Odbiorca(std::string imie_O, std::string nazwisko_O, std::string mail_O, std::string ulica_O, std::string miasto_O, std::string wojewodztwo_O, int kod_pocztowy_O, std::string kraj_O) 
+	{
+		imie(imie_O);
+		nazwisko(nazwisko_O);
+		adres_mailowy(mail_O);
+		ulica(ulica_O),
+		miasto(miasto_O),
+		wojewodztwo(wojewodztwo_O),
+		kod_pocztowy(kod_pocztowy_O),
+		kraj(kraj_O);
+	}
+	*/
+};
+
+class Przesylka : public Odbiorca
+{
+public:
+	std::string numer_przesylki;
+	std::string wymiar;
+	virtual void Przesylka() = 0; 
+};
+
+class  Duza_Paczka : public Przesylka
+{
+public:
+	int waga;
+
+};
+
+
+
+
+
+
+	
+
+
+
+
+
+class MalaPaczka :public Przesylka
+{
+public:
+
+};
+
+class Srednia Paczka :public Przesylka
+{
+
 };
