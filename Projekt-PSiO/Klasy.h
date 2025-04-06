38a89dbb7d01c8079e46e685787388eaec2f551c
Kamil <bijak.kamil@wp.pl>
2025-04-06 17:41:20 +0200

    Nadawca

--- Projekt-PSiO/Klasy.h
+++ Projekt-PSiO/Klasy.h
@@ -23,6 +23,15 @@ public:
 class Nadawca : public Osoba,  public Adres
 {
 public:
-	Nadawca(std::string imie, std::string nazwisko, std::string mail, std::string   ) {}
+	Nadawca(std::string imie_N, std::string nazwisko_N std::string mail_N, std::string ulica_N, std::string miasto_N, std::string wojewodztwo_N, int kod_pocztowy_N, std::string kraj_N) 
+	{
+		imie_N(imie), nazwisko_N(nazwisko), adres_mailowy_M(mail), ulica_N(ulica), miasto_N(miasto), wojewodztwo_N(wojewodztwo), kod_pocztowy_N(kod_pocztowy), kraj_N(kraj);
+	}
+	int numer_nadawczy;
+};
+class Odbiorca : public Osoba, public Adres
+{
+public:
+	Odbiorca(std::string imie, std::string nazwisko, std::string mail, std::string ulica, std::string miasto, std::string wojewodztwo, int kod_pocztowy, std::string kraj) {}
 	int numer_nadawczy;
 };
