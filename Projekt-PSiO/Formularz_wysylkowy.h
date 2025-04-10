efba74462238a6c1b2c333a67c449d4037e0044b
Kamil <bijak.kamil@wp.pl>
2025-04-10 13:44:26 +0200

    pole tektowe

--- Projekt-PSiO/Formularz_wysylkowy.h
+++ Projekt-PSiO/Formularz_wysylkowy.h
@@ -1,65 +1,93 @@
 #pragma once
+#include <string>
+#include <msclr/marshal_cppstd.h> 
 
 namespace ProjektPSiO {
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+    public ref class MyForm1 : public System::Windows::Forms::Form {
+    public:
+        MyForm1(void) {
+            InitializeComponent();
+        }
 
-	/// <summary>
-	/// Podsumowanie informacji o MyForm1
-	/// </summary>
-	public ref class MyForm1 : public System::Windows::Forms::Form
-	{
-	public:
-		MyForm1(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: W tym miejscu dodaj kod konstruktora
-			//
-		}
+    protected:
+        ~MyForm1() {
+            if (components) delete components;
+        }
 
-	protected:
-		/// <summary>
-		/// Wyczyœæ wszystkie u¿ywane zasoby.
-		/// </summary>
-		~MyForm1()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+    private:
+        System::Windows::Forms::Button^ button1;
+        System::Windows::Forms::Timer^ timer1;
+        System::Windows::Forms::TextBox^ textBox1;
 
-	private:
-		/// <summary>
-		/// Wymagana zmienna projektanta.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+        System::ComponentModel::IContainer^ components;
 
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Metoda wymagana do obs³ugi projektanta — nie nale¿y modyfikowaæ
-		/// jej zawartoœci w edytorze kodu.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->SuspendLayout();
-			// 
-			// MyForm1
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(784, 933);
-			this->Name = L"MyForm1";
-			this->Text = L"MyForm1";
-			this->ResumeLayout(false);
+        void InitializeComponent(void) {
+            this->components = (gcnew System::ComponentModel::Container());
+            this->button1 = (gcnew System::Windows::Forms::Button());
+            this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+            this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+            this->SuspendLayout();
+            // 
+            // button1
+            // 
+            this->button1->Location = System::Drawing::Point(175, 87);
+            this->button1->Name = L"button1";
+            this->button1->Size = System::Drawing::Size(75, 23);
+            this->button1->TabIndex = 0;
+            this->button1->Text = L"button1";
+            this->button1->UseVisualStyleBackColor = true;
+            this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
+            // 
+            // textBox1
+            // 
+            this->textBox1->Location = System::Drawing::Point(246, 461);
+            this->textBox1->Name = L"textBox1";
+            this->textBox1->Size = System::Drawing::Size(100, 20);
+            this->textBox1->TabIndex = 1;
+            this->textBox1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm1::textBox1_KeyDown);
+            // 
+            // MyForm1
+            // 
+            this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->ClientSize = System::Drawing::Size(784, 933);
+            this->Controls->Add(this->textBox1);
+            this->Controls->Add(this->button1);
+            this->Name = L"MyForm1";
+            this->Text = L"MyForm1";
+            this->ResumeLayout(false);
+            this->PerformLayout();
 
-		}
-#pragma endregion
-	};
+        }
+
+    private:
+        System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+            if (String::IsNullOrEmpty(textBox1->Text)) {
+                MessageBox::Show("Pole jest puste!", "B³¹d");
+            }
+            else {
+                MessageBox::Show("Wpisa³eœ: " + textBox1->Text, "Info");
+            }
+        }
+
+        System::Void textBox1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+            if (e->KeyCode == Keys::Enter) {
+                button1_Click(sender, e); // Reu¿ywa logiki z przycisku
+                e->SuppressKeyPress = true;
+            }
+        }
+
+        
+        System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {}
+    private: System::Void textBox2_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
+    }
+
+    };
 }
--- Projekt-PSiO/MyForm.h
+++ Projekt-PSiO/MyForm.h
@@ -37,6 +37,10 @@ namespace ProjektPSiO {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+
+
 	protected:
 
 	protected:
@@ -58,6 +62,8 @@ namespace ProjektPSiO {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -70,17 +76,40 @@ namespace ProjektPSiO {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(66, 90);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(243, 160);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"test";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1716, 24);
+			this->menuStrip1->TabIndex = 2;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ControlDark;
 			this->ClientSize = System::Drawing::Size(1716, 891);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
 			this->Text = L"E-sortownia v1.0";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -91,6 +120,12 @@ namespace ProjektPSiO {
 		// Uruchomienie aplikacji
 		form1->Show();
 	}
-	};
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
 
