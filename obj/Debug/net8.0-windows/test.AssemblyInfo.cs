d6fdfe6a45dba2cc53ef92c341e05b4cf4a98f36
Kamil Bijak <bijak.kamil@wp.pl>
2025-05-11 12:07:09 +0200

    proba 2

--- obj/Debug/net8.0-windows/test.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336a3116c3a93c74e3a3c9dc9a2ec478ab1c775e")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
