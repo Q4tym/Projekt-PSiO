ec8030ce6f5879aa1bce4891ada6804c3bd7bbc4
Kamil Bijak <bijak.kamil@wp.pl>
2025-05-11 12:08:59 +0200

    hello

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace test
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            MessageBox.Show("Hello, World!");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
